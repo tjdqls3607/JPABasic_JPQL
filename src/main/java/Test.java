7915b35d5c38f4ec9eaf80a88e723dc74662f41e
정성빈 <tjdqls3124@gmail.com>
2025-03-13 14:38:19 +0900

    named parameter

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,6 +4,7 @@ import entity.Employee;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
 import org.hibernate.jpa.HibernatePersistenceProvider;
 
 import java.util.HashMap;
@@ -27,11 +28,36 @@ public class Test {
 
 //		em.getTransaction().begin();
 
-		// #1 nomal query
+		// #1 normal query
+//		{
+//			// SQL : Select * from employee
+//			String jpql = "select e from Employee e";	// Entity 를 이용한 query 형식
+//			Query query = em.createQuery(jpql);
+//			List<Employee> list = query.getResultList();
+//
+//			for (Employee employee : list) {
+//				System.out.println(employee);
+//			}
+//		}
+
+		// #2 typed query
+//		{
+//			// SQL : Select * from employee
+//			String jpql = "select e from Employee e";	// Entity 를 이용한 query 형식
+//			TypedQuery<Employee> query = em.createQuery(jpql, Employee.class);
+//			List<Employee> list = query.getResultList();
+//
+//			for (Employee employee : list) {
+//				System.out.println(employee);
+//			}
+//		}
+
+		// #3 parameter
 		{
-			// SQL : Select * from employee
-			String jpql = "select e from Employee e";	// Entity 를 이용한 query 형식
-			Query query = em.createQuery(jpql);
+			// SQL : "Select * from employee where id = 3"
+			String jpql = "select e from Employee e where id >= :id";	// Entity 를 이용한 query 형식
+			TypedQuery<Employee> query = em.createQuery(jpql, Employee.class);
+			query.setParameter("id", 2);
 			List<Employee> list = query.getResultList();
 
 			for (Employee employee : list) {
